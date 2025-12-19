6859af5b59da2ea37fdc4012bed8c47278a38c9e
Amiurr <145832953+amiamiur@users.noreply.github.com>
2025-12-19 17:32:38 +0300

    efactor habits router to use  and validate owner

--- app/routers/habits.py
+++ app/routers/habits.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -11,23 +11,32 @@ router = APIRouter(
     tags=["Habits"]
 )
 
-fake_habits_db = []
 
-
-@router.post("/", response_model=HabitResponse)
+@router.post("/", response_model=HabitResponse, status_code=status.HTTP_201_CREATED)
 def create_habit(
     habit: HabitCreate,
     owner_id: int,
     db: Session = Depends(get_db),
 ):
+    # Проверяем, существует ли пользователь
+    user = db.query(models.User).filter(models.User.id == owner_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
     db_habit = models.Habit(
         title=habit.title,
         description=habit.description,
+        is_active=habit.is_active,
         owner_id=owner_id
     )
+
     db.add(db_habit)
     db.commit()
     db.refresh(db_habit)
+
     return db_habit
 
 
@@ -36,4 +45,3 @@ def get_habits(
     db: Session = Depends(get_db),
 ):
     return db.query(models.Habit).all()
-
