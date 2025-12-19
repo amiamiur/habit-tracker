06443d576f22cd56e64dfbec8b24e8fff5314a39
Amiurr <145832953+amiamiur@users.noreply.github.com>
2025-12-19 17:40:56 +0300

    dd abitog model and relationship with abit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy import Date
+from datetime import date
 
 from app.database import Base
 
@@ -25,8 +27,11 @@ class Habit(Base):
     owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     owner = relationship("User", back_populates="habits")
 
-    logs = relationship("HabitLog", back_populates="habit", cascade="all, delete")
-
+    logs = relationship(
+        "HabitLog",
+        back_populates="habit",
+        cascade="all, delete"
+    )
 
 class HabitLog(Base):
     __tablename__ = "habit_logs"
@@ -35,4 +40,14 @@ class HabitLog(Base):
     date = Column(Date, nullable=False)
 
     habit_id = Column(Integer, ForeignKey("habits.id"), nullable=False)
+    habit = relationship("Habit", back_populates="logs")
+
+class HabitLog(Base):
+    __tablename__ = "habit_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    habit_id = Column(Integer, ForeignKey("habits.id"), nullable=False)
+    date = Column(Date, default=date.today, nullable=False)
+    completed = Column(Boolean, default=True)
+
     habit = relationship("Habit", back_populates="logs")
