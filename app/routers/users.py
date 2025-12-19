381d084d4159dee9b969fc39e48bfc5ff58ca65d
Amiurr <145832953+amiamiur@users.noreply.github.com>
2025-12-19 17:24:47 +0300

    dd database-backed users router with basic

--- app/routers/users.py
+++ app/routers/users.py
@@ -43,4 +43,16 @@ def create_user(
 def get_users(db: Session = Depends(get_db)):
     return db.query(models.User).all()
 
+@router.get("/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return user
+
 
