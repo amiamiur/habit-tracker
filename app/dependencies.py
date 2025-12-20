09e1fed5e15760bb782c386e7a3da4e5b2956c39
Amiurr <145832953+amiamiur@users.noreply.github.com>
2025-12-20 22:05:32 +0300

    Add get_current_user dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,9 @@
 from app.database import SessionLocal
 from sqlalchemy.orm import Session
 from typing import Generator
+from fastapi import Depends, HTTPException
+from app.database import SessionLocal
+from app import models
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -9,3 +12,9 @@ def get_db() -> Generator[Session, None, None]:
         yield db
     finally:
         db.close()
+
+def get_current_user(db: Session = Depends(get_db)) -> models.User:
+    user = db.query(models.User).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    return user
