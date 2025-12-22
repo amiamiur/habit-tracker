969d3956fc9635ba207ea58a013b0809d15c59ec
Amiurr <145832953+amiamiur@users.noreply.github.com>
2025-12-22 16:52:49 +0300

    create_table needed only for developments

--- app/create_table.py
+++ app/create_table.py
@@ -1,5 +1,5 @@
-from app.database import engine
-from app import models
-
-models.Base.metadata.create_all(bind=engine)
-print("Tables created")
+# from app.database import engine
+# from app import models
+#
+# models.Base.metadata.create_all(bind=engine)
+# print("Tables created")
