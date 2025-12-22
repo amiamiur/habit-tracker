0f0ac194ce3807578d9df5e38d18bbcca2780842
Amiurr <145832953+amiamiur@users.noreply.github.com>
2025-12-22 16:50:21 +0300

    chore: add production requirements

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 from app.database import engine
 from app import models
@@ -11,9 +12,18 @@ app = FastAPI(
     version="0.1.0"
 )
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
 def root():
     return {"message": "Habit Tracker API is running"}
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
 
 
 app.include_router(users.router)
