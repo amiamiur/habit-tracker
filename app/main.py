a31f19e242b327d0b3b770c7076f79fca1d878a7
Amiurr <145832953+amiamiur@users.noreply.github.com>
2025-12-22 17:59:38 +0300

    Include auth router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.database import engine
 from app import models
 from app.routers import users, habits, auth, habit_logs
@@ -11,26 +10,12 @@ app = FastAPI(
     version="0.1.0"
 )
 
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def root():
     return {"message": "Habit Tracker API is running"}
-
-@app.get("/")
-def index(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
-
-
 app.include_router(users.router)
 app.include_router(habits.router)
 app.include_router(habit_logs.router)
 app.include_router(auth.router)
-
-@app.on_event("startup")
-def on_startup():
-    # Создаём таблицы при старте (только для разработки)
-    models.Base.metadata.create_all(bind=engine)
